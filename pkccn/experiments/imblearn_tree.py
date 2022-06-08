038b3b47cc1baf910335c525379e11ca04177907
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2022-06-08 14:11:34 +0200

    Revert "Store imblear_tree results after every repetition of all data sets"
    This reverts commit 50260d7a7af3fe64dfd58b317574ea413f2bd043.

--- pkccn/experiments/imblearn_tree.py
+++ pkccn/experiments/imblearn_tree.py
@@ -15,32 +15,13 @@ from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import StratifiedKFold
 from tqdm.auto import tqdm
 
-def _store(results, path, p_minus, p_plus):
-    """Aggregate and store the results"""
-    df = pd.DataFrame(results)
-    df = df.groupby(["dataset", "method"], sort=False).agg(
-        threshold = ("threshold", "mean"),
-        threshold_std = ("threshold", "std"),
-        accuracy = ("accuracy", "mean"),
-        accuracy_std = ("accuracy", "std"),
-        f1 = ("f1", "mean"),
-        f1_std = ("f1", "std"),
-        lima = ("lima", "mean"),
-        lima_std = ("lima", "std"),
-    )
-    print(df.groupby("method", sort=False).agg(f1 = ("f1", "mean")))
-    df['p_minus'] = p_minus
-    df['p_plus'] = p_plus
-    df.to_csv(path)
-    print(f"{df.shape[0]} results succesfully stored at {path}")
-
 def main(
         output_path,
         p_minus,
         p_plus,
         seed = 867,
         n_folds = 10,
-        n_repetitions = 20,
+        n_repetitions = 5,
         is_test_run = False,
     ):
     print(f"Starting an imblearn_tree experiment to produce {output_path} with seed {seed}")
@@ -56,20 +37,22 @@ def main(
         clf.max_depth = 4
         clf.n_estimators = 32
 
-    # iterate over all repetitions; parallelize via LiMaRandomForest
+    # iterate over all data sets
     results = []
     trial_seeds = np.random.randint(np.iinfo(np.uint32).max, size=n_repetitions)
-    for i_trial, trial_seed in enumerate(trial_seeds):
+    for i_dataset, dataset in enumerate(datasets(is_test_run)):
+        imblearn_dataset = fetch_datasets()[dataset]
+        X = imblearn_dataset.data
+        y = imblearn_dataset.target
+
+        # iterate over all repetitions; parallelize via LiMaRandomForest
         progressbar = tqdm(
-            datasets(is_test_run), # iterate over all data sets
-            desc = f"Repetition {i_trial+1}/{len(trial_seeds)}",
-            total = len(datasets(is_test_run)),
+            trial_seeds, # each trial gets a different seed
+            desc = f"{dataset} [{i_dataset+1}/{len(datasets(is_test_run))}]",
+            total = n_repetitions,
             ncols = 80
         )
-        for dataset in progressbar:
-            imblearn_dataset = fetch_datasets()[dataset]
-            X = imblearn_dataset.data
-            y = imblearn_dataset.target
+        for trial_seed in progressbar:
             np.random.seed(trial_seed)
             y_ccn = inject_ccn(y, p_minus, p_plus)
 
@@ -89,8 +72,24 @@ def main(
                 "f1": f1_score(y, y_pred),
                 "lima": lima_score(y_ccn, y_pred, p_minus), # noisy LiMa
             })
-        _store(results, output_path.replace(".csv", ".tmp.csv"), p_minus, p_plus)
-    _store(results, output_path, p_minus, p_plus) # store all results
+
+    # aggregate and store the results
+    df = pd.DataFrame(results)
+    df = df.groupby(["dataset", "method"], sort=False).agg(
+        threshold = ("threshold", "mean"),
+        threshold_std = ("threshold", "std"),
+        accuracy = ("accuracy", "mean"),
+        accuracy_std = ("accuracy", "std"),
+        f1 = ("f1", "mean"),
+        f1_std = ("f1", "std"),
+        lima = ("lima", "mean"),
+        lima_std = ("lima", "std"),
+    )
+    print(df.groupby("method", sort=False).agg(f1 = ("f1", "mean")))
+    df['p_minus'] = p_minus
+    df['p_plus'] = p_plus
+    df.to_csv(output_path)
+    print(f"{df.shape[0]} results succesfully stored at {output_path}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -101,8 +100,8 @@ if __name__ == '__main__':
                         help='random number generator seed (default: 876)')
     parser.add_argument('--n_folds', type=int, default=10, metavar='N',
                         help='number of cross validation folds (default: 10)')
-    parser.add_argument('--n_repetitions', type=int, default=20, metavar='N',
-                        help='number of repetitions of the cross validation (default: 20)')
+    parser.add_argument('--n_repetitions', type=int, default=5, metavar='N',
+                        help='number of repetitions of the cross validation (default: 5)')
     parser.add_argument("--is_test_run", action="store_true")
     args = parser.parse_args()
     main(
